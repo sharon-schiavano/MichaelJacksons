2e4aecd6310dc36f1f2be8be5b7cdfa62f744706
Omar Silano <silanoomar@gmail.com>
2025-02-03 17:03:01 +0100

    Update login-manager.php
    migliorato il codice

--- login-register/login-manager.php
+++ login-register/login-manager.php
@@ -10,90 +10,60 @@
 
     $db = pg_connect($connection_string) or die('Impossibile connettersi al database: ' . pg_last_error());
 
+	$input = pg_escape_string($db,$_POST["usernamelogin"]);
 
 	$password = $_POST["passwordlogin"];
 
-	if (controlloInput($_POST["usernamelogin"]) > -1) {		//ho inserito in questo campo una e-mail
-
-		$email = pg_escape_string($db,$_POST["usernamelogin"]);
-
+	if ( controlloInput($input) > -1 )		//ho inserito in questo campo una e-mail
 		$sql = "SELECT * FROM utenti WHERE email = $1 ";
 
-		$ret = pg_prepare($db,"selezionaUtente",$sql);
-
-		if (!$ret) {
-			echo "Errore durante il riconoscimento utente tramite e-mail :" . pg_last_error($db);
-		} else {
-
-				$ret = pg_execute($db,"selezionaUtente",array($email));
-
-				if (pg_num_rows($ret) == 0) {
-
-					echo "Errore: L'email $email non è associata ad alcun account";
-
-				} else {
-					
-					$row = pg_fetch_assoc($ret);
-
-					if ( password_verify($password,$row['password']) ) {
-
-						session_start();
-
-						$_SESSION['username'] = $row['username'];
-
-						header("Location: ../homepage/UtenteRegistrato.html");
-					}
-
-					else {
-						echo "Errore: Password errata";
-					}
-
-			}
-
-
-		}
-
-	} else {		//ho inserito uno username
-
-		$username = pg_escape_string($db,$_POST["usernamelogin"]);
-
+	else 	
 		$sql = "SELECT * FROM utenti WHERE username = $1 ";
 
-		$ret = pg_prepare($db,"selezionaUtente",$sql);
+	$ret = pg_prepare($db, "selezionaUtente", $sql);
 
-		if (!$ret) {
-			echo "Errore durante il riconoscimento utente tramite username :" . pg_last_error($db);
-		} else {
+	if (!$ret) {
 
-			$ret = pg_execute($db,"selezionaUtente",array($username));
-
-			if (pg_num_rows($ret) == 0) {
-
-				echo "Errore: Utente $username non trovato";
-
-			} else {
-
-				$row = pg_fetch_assoc($ret);
-
-				if ( password_verify($password,$row['password']) ) {
-
-					session_start();
-
-					$_SESSION['username'] = $row['username'];
-
-					header("Location: ../homepage/UtenteRegistrato.html");
-
-				}
-
-				else {
-					
-					echo "Errore: Password errata";
-				}
-
-			}
-		}
+		echo "Errore nella preparazione della query! " . pg_last_error();
+		exit;
 
 	}
+
+
+
+	$ret = pg_execute($db,"selezionaUtente",array($input));
+
+	if (pg_num_rows($ret) == 0) {
+
+		if (controlloInput($input) > -1)
+			echo "Errore: l'email $input non è associata ad alcun account!";
+
+		else 
+			echo "Errore: username $input non esiste!";
+
+		exit;
+		}
+	
+	
+	$row = pg_fetch_assoc($ret);
+
+	if ( password_verify ($password, $row['password'] ) ) {
+
+		session_start();
+
+		$_SESSION['username'] = $row['username'];
+
+		header("Location: ../homepage/UtenteRegistrato.html");
+
+	} else {
+		echo "Errore: password errata!";
+	}
+
+	
+
+	
+
+	 
 	
 
 ?>
