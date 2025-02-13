c096f622f5f3e5a8a1575ce83d1a0c1c973d23b9
Omar Silano <silanoomar@gmail.com>
2025-02-13 23:07:08 +0100

    Add files via upload

---
+++ login-register/config.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = 'localhost';
+    $port = '5432';
+    $dbname = 'gruppo07';
+    $user = 'www';
+    $password = 'tw2024';
+
+    $connection_string = "host=$host port=$port dbname=$dbname user=$user password=$password";
+
+    $db=pg_connect($connection_string) or die('Impossibile connettersi al database: ' . pg_last_error()); 
+
+?>
