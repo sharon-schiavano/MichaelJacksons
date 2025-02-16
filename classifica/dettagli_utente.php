25e8b3e51c7ca5873f30580da8a038643776207f
vincenzonunziato <chippolone3393@gmail.com>
2025-02-16 13:37:11 +0100

    Update dettagli_utente.php

--- classifica/dettagli_utente.php
+++ classifica/dettagli_utente.php
@@ -1,11 +1,32 @@
 <?php
 include '../php_in_comune/config.php';
 
+// Connessione al database
+$connection_string = "host=$host port=$port dbname=$dbname user=$user password=$password";
+$db = pg_connect($connection_string);
+
+if (!$db) {
+    echo json_encode(["success" => false, "error" => "Errore di connessione al database"]);
+    exit;
+}
+
 if (isset($_GET['id'])) {
     $id = intval($_GET['id']);
-    $query = "SELECT username, email, mjc, immagine_profilo, data_iscrizione, valuta, billie_jean, beat_it, rock_with_you, smooth_criminal, thriller FROM utenti WHERE id = ?";
-    $stmt = $pdo->prepare($query);
-    $stmt->execute([$id]);
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+    $query = "SELECT username, email, mjc, immagine_profilo, data_iscrizione, valuta, billie_jean, beat_it, rock_with_you, smooth_criminal, thriller 
+              FROM utenti 
+              WHERE id = $1";
+
+    $result = pg_query_params($db, $query, [$id]);
+
+    if (!$result) {
+        echo json_encode(["success" => false, "error" => "Errore nell'esecuzione della query"]);
+        exit;
+    }
+
+    $user = pg_fetch_assoc($result);
+    pg_close($db);
+    
     echo json_encode($user);
 }
+?>
